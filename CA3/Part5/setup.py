1fbbeda9ec60649ec9b13f4f6987fba6b9948e8e
LuisDavid0912 <157650947+LuisDavid0912@users.noreply.github.com>
2024-03-31 19:04:01 -0600

    Delete CA3 directory

--- CA3/Part5/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(ext_modules=cythonize(["Cython1.py",
-                            "Cython2.py",
-                            "Cython3.py",
-                            "Cython4.py",
-                                ])
-)
